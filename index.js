fde86e1c3a21ba6d8419dfd1f8a76cf432851cf5
KanchanaCJagodage <kanchanachamodanijagodage@gmail.com>
2021-09-12 19:40:55 +0530

    adding changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var http = require('http');
-const { listenerCount } = require('process');
+
 
 http.createServer(function(res,req){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.write('Hello World');
+    res.end();
 });
 
 listen(3000);
